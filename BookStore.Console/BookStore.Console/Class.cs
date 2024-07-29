79aee0584903b1cbab8fa814b3a6e96b5dbdf460
Esmaeel <nova99trex@gmail.com>
2024-07-29 21:52:01 +0330

    Update Class.cs

--- BookStore.Console/BookStore.Console/Class.cs
+++ BookStore.Console/BookStore.Console/Class.cs
@@ -1,14 +1,33 @@
 public class BookStore 
 {
+     public string Name { get; set; }
+    public string Address { get; set; }
+    public bool HasCafe { get; set; }
+    public bool HasStudyPlace { get; set; }
+    public bool HasStationery { get; set; }
+
     public BookStore(string name)
     {
         Name = name;
+        Address = "";
+        HasCafe = false;
+        HasStudyPlace = false;
+        HasStationery = false;
     }
 
-    public string Name { get; set; }
+    public void SellBooks()
+    {
+        Console.WriteLine($"Selling Book in {Name} BookStore ");
+    }
+
 
     public void Advice(string customerName)
     {
-        Console.WriteLine($"Wellcome To Blue Book Store {customerName}");
+        Console.WriteLine($"Helping {customerName} To Choose a Book For Read in {Name} BookStore ");
+    }
+
+    public void QueForReadingEvents()
+    {
+        Console.WriteLine($"Queuing Users For the Reading Event in {Name} BookStore");
     }
 }
