9e59f7440b55e52e4552fffad821895b4facee06
Esmaeel <nova99trex@gmail.com>
2024-07-29 21:52:24 +0330

    New Upload Program.cs

--- BookStore.Console/BookStore.Console/Program.cs
+++ BookStore.Console/BookStore.Console/Program.cs
@@ -1,4 +1,21 @@
-﻿
-BookStore blueBookStore = new BookStore("Blue");
-Console.WriteLine($"Book Store {blueBookStore.Name} Opened!");
-blueBookStore.Advice("Esmaeel");
+class Program 
+{
+    static void Main()
+    {
+        BookStore blueBookStore = new BookStore("Blue");
+
+        blueBookStore.SellBooks();
+        blueBookStore.Advice("Esmaeel");
+        blueBookStore.QueForReadingEvents();
+        blueBookStore.Address = "Shiraz/Qodoosi";
+        blueBookStore.HasCafe = true;
+        blueBookStore.HasStudyPlace = true;
+        blueBookStore.HasStationery = true;
+
+        Console.WriteLine($"Book Store {blueBookStore.Name} Opened!");
+        Console.WriteLine($"{blueBookStore.Name} Book Store is in Address : \"{blueBookStore.Address}\"");
+        Console.WriteLine($"Is This Book Store Has Cafe ? {blueBookStore.HasCafe}");
+        Console.WriteLine($"Is This Book Store Has Study For Place ? {blueBookStore.HasStudyPlace}");
+        Console.WriteLine($"Is This Book Store Has Stationery ? {blueBookStore.HasStationery}");
+    }
+}
