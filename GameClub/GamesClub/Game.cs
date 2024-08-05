cd34178c41db046a5652406e911f5744f78a2615
Esmaeel <nova99trex@gmail.com>
2024-08-06 01:00:18 +0330

    Delete GameClub directory

--- GameClub/GamesClub/Game.cs
+++
@@ -1,6 +0,0 @@
-public abstract class Game
-{
-    public abstract string Name { get; }
-    public virtual string Description => string.Empty;
-    public abstract void Play();
-}
--- GameClub/GamesClub/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿var GuessNumberGame = new GuessNumber("Guess Number");
-var GuessWordGame = new GuessWord("Guess Word");
-
-var gameClub = new GameClub(GuessNumberGame, GuessWordGame);
-
-gameClub.ComeIn("Esmaeel");
